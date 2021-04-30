bc4eeb422b2202ebf4cc2ff92e6f4499a9610edd
Tom Fream <tomfream@gmail.com>
2021-04-29 19:33:37 -0600

    Implements fetch user reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,7 +90,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT * FROM reservations WHERE guest_id = $1 LIMIT $2;
+  `, [guest_id, limit])
+  .then((result) => {
+    if (result) {
+      return result.rows;
+    } else return null;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
